690ba23fc4b77419b27a1d8319366303df760e78
ritachen001 <rita84328@gmail.com>
2020-09-27 15:04:47 +0800

    upload

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 let express = require('express');
 let bodyParser = require('body-parser');
-const url = require('url');
 let querystring = require('querystring');
 let cookieParser = require('cookie-parser');
-// var getDataRouter = require( ' ./routes/getData ' );
 
 
 let app = express();
@@ -19,17 +17,58 @@ let mainRoutes = require('./routes');
 
 app.use(mainRoutes);
 // app.use('/cards', cardRoutes);
-// app.use( ' /getData ' , getDataRouter);
 
 
 
-
+//appworks mission - Assignment 1: 
 //讓localhost 吃到index資料
 app.get('/' , (req , res) => {
     res.render('index');
 });
 
-//Assignment 4: HTTP Cookie
+
+
+
+
+
+//appworks mission - Assignment 2: 
+//瀏覽器網址：http://localhost:3000/getData?number=5
+app.get('/getData', (req , res) => {
+    let number = req.query.number;
+    res.send(inputData(number));
+    // inputData(number);
+	// console.log(outputData);
+});
+
+function isInteger(number){
+    return number %1 == 0;
+}
+
+function inputData(number) { 
+    let alert="Lack of Parameter"; //預設為空值
+    if (number !== null && number !== undefined && number !== "") 
+    {
+        if( isInteger(number) === true){ //判斷為正整數  //else if(typeof(number)=='number')
+          let max = number;
+          let sum = 0;
+        //   message ="";
+            for (let i = 1; i <= max; i++) {
+                    sum += i;  // /getData?number=正整數
+                }
+            alert = ""+sum ;
+        } 
+    
+        else  {  //判斷為字串  //if (typeof(number)=='string')
+
+        alert = "Wrong Parameter";
+        }
+    }    
+        return alert;
+}
+
+
+    
+//appworks mission - Assignment 4: HTTP Cookie
 
 app.get('/myName' , (req , res) => {
     res.render('hello');
@@ -43,61 +82,13 @@ app.post('/myName' , (req , res) => {
 
 
 
-//appworks mission - Assignment 2: 
 
-
-//瀏覽器網址：http://localhost:3000/getData?number=5
-app.get('/getData', (req , res) => {
-    let number = req.query.number;
-    res.end(inputData(number));
-    if(typeof(number)=='number'){
-        number = 1, number++
-
-    };
-
-});
-
-
-
-
-
-// document.write(typeof(number));　
-
-function inputData(number) { 
-  
-    let alert="Lack of Parameter"; //預設為空值
-  
-    if(typeof(number)=='string'){  //判斷為字串
-      alert="Wrong Parameter";
-      }else if(typeof(number)=='number'){ //判斷為正整數
-          // [[  算總和  ]]
-          let max = number;
-          let sum = 0;
-          for (let i = 1; i < max; i++) {
-                  sum += i;
-                   // /getData?number=正整數
-              }
-          alert = sum;
-      }
-     return alert;
-  }
-
-
-
-
-
-
-app.get('/endpoint', function(request, response) {
-    let name = request.query.name;
-    response.end("I have received the ID: " + name);
-});
+// app.get('/endpoint', function(request, response) {
+//     let name = request.query.name;
+//     response.end("I have received the ID: " + name);
+// });
 //在瀏覽器中訪問： http://localhost:3000/endpoint?id=something
 
-
-// app.get('/hi/:param1', function(req,res){
-//     let {qs1: "you" , qs2: "tube"} = req.query;
-//     let {param1: "there"} = req.params;
-// } );
 
 
 
